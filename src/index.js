a40a5b6fd46c175d7fcc2845540c0b028d00d226
naimish <62000943+naimish73@users.noreply.github.com>
2023-04-03 03:06:49 +0530

    odering of pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-// react imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter,
 } from 'react-router-dom';
 
-// image and other library imports
-
-// css imports
-
-// pages imports
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
